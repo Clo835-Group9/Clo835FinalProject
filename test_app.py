a51f37247d21bacb5de34c07c5a8038a2d579210
PoonamAgarwal <pragarwal@myseneca.ca>
2025-04-07 22:33:42 +0000

    added test file for unit testing in gt actions

--- test_app.py
+++ test_app.py
@@ -1,8 +1,18 @@
-# test_app.py
+import pytest
 from app import app
 
-def test_home():
-    tester = app.test_client()
-    response = tester.get("/")
-    assert response.status_code == 200
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Secure. Scalable. Cloud." in response.data  
+
+def test_about_page(client):
+    response = client.get('/about')
+    assert response.status_code == 200
+    assert b"Secure. Scalable. Cloud." in response.data
